0126bde6454d7dd596451566d906c615443aeb2d
KazuyaMishima <ingdantevi@hotmail.com>
2018-04-24 23:18:44 +0200

    iniciopasd
    iniopasd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {
+  AuthService,
+  FacebookLoginProvider,
+  GoogleLoginProvider
+} from 'angular5-social-login';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>
+    Sign in
+  </h1>
+
+  <button (click)="socialSignIn('facebook')">Sign in with Facebook</button>
+  <button (click)="socialSignIn('google')">Signin in with Google</button>  `,
+  /*styleUrls: ['./signin.component.css']*/
 })
-export class AppComponent {
-  title = 'app';
+
+
+export class AppComponent implements OnInit {
+
+  constructor( private socialAuthService: AuthService ) {}
+
+  public socialSignIn(socialPlatform : string) {
+    let socialPlatformProvider;
+    if(socialPlatform == "facebook"){
+      socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
+    }
+
+    this.socialAuthService.signIn(socialPlatformProvider).then(
+      (userData) => {
+        console.log(socialPlatform+" sign in data : " , userData);
+        // Now sign-in with userData
+
+      }
+    );
+  }
+
+  ngOnInit(){
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import {
+  SocialLoginModule,
+  AuthServiceConfig,
+
+  FacebookLoginProvider,
+} from 'angular5-social-login';
+
+export function getAuthServiceConfigs() {
+  let config = new AuthServiceConfig(
+    [
+      {
+        id: FacebookLoginProvider.PROVIDER_ID,
+        provider: new FacebookLoginProvider("185039692117718")
+      },
+    ]);
+  return config;
+}
 
 
 @NgModule({
@@ -10,9 +25,15 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocialLoginModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: AuthServiceConfig,
+      useFactory: getAuthServiceConfigs
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
